22a5116ba5d2c2623724517339bd673af37f2305
nonqme <w.missary@gmail.com>
2025-04-08 08:58:36 +0200

    chore: simple screenshot test

--- src/core/commands/record.ts
+++ src/core/commands/record.ts
@@ -1,3 +1,8 @@
+import path from 'node:path';
+import fs from 'node:fs';
+
+import screenshot from 'screenshot-desktop';
+
 export type RecordOptions = {
   name: string;
   fps: number;
@@ -5,12 +10,41 @@ export type RecordOptions = {
   output: string;
 };
 
-export default function record(options: RecordOptions) {
+export default async function record(options: RecordOptions) {
   const { name, fps, window, output } = options;
 
-  console.log('Recording a screenshot dataset with the following options:');
-  console.log(`Name: ${name}`);
-  console.log(`FPS: ${fps}`);
-  console.log(`Window: ${window}`);
-  console.log(`Output: ${output}`);
+  if (!name) {
+    throw new Error('Name is required');
+  }
+  if (!fps) {
+    throw new Error('FPS is required');
+  }
+  if (!window) {
+    throw new Error('Window name is required');
+  }
+  if (!output) {
+    throw new Error('Output directory is required');
+  }
+
+  const outputDir = path.resolve(output);
+  if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir, { recursive: true });
+  }
+  let screenNumber = 0;
+  // Capture the screenshots
+  const interval = 1000 / fps;
+  setInterval(async () => {
+    try {
+      const img = await screenshot({
+        format: 'png',
+      });
+      const fileName = `${name}-${Date.now()}.png`;
+      fs.writeFileSync(path.join(outputDir, fileName), img);
+      screenNumber++;
+      console.log(screenNumber);
+    } catch (error) {
+      console.error('Error capturing screenshot:', error);
+    }
+  }, interval);
+  console.log(`Capturing screenshots every ${interval}ms...`);
 }
