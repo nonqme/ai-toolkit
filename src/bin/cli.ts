93cc5fab627751d453a349f88ae2de8d1359a6d9
nonqme <w.missary@gmail.com>
2025-04-14 17:54:19 +0200

    chore: rework branch

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -1,29 +1,22 @@
 #!/usr/bin/env node
 import sade from 'sade';
 
-import captureImage from '../commands/capture-image.js';
-
 const program = sade('ai');
 
 program.version('0.0.1').describe('An AI CLI for various tasks');
 
 program
-  .command('capture-image')
+  .command('capture')
   .describe(
-    'Capture a set of images from the screen and save them to a directory'
+    'Capture a set of images or a video from the screen and save them to a directory'
   )
   .option('-n, --name', 'Name of the set')
   .option('-w, --window', 'Window name to capture')
   .option('-f, --fps', 'Number of screenshots per second')
   .option('-o, --output', 'Output directory for the captured images')
+  .option('-t, --type', 'Type of capture (image or video)')
   .action(async (options) => {
-    const { name, window, fps, output } = options;
-    await captureImage({
-      name,
-      window,
-      fps,
-      output,
-    });
+    console.log(options);
   });
 
 program.parse(process.argv);
